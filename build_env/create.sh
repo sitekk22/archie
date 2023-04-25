684df90ecdd9971875ae3104d3befa305c1e5586
sitekk22 <sitarzkam100@gmail.com>
2023-04-25 15:52:02 +0200

    init scripts

---
+++ build_env/create.sh
@@ -0,0 +1,9 @@
+echo "Dist Root: ${DIST_ROOT:?}"
+echo "LFS: ${LFS:?}"
+
+if ! test $(whoami) == "lfs"; then
+  echo "Must run as lfs"
+  exit -1
+fi
+
+echo "Creating build environment..."
---
+++ build_env/prepare.sh
@@ -0,0 +1,51 @@
+echo "Dist Root: ${DIST_ROOT:?}"
+echo "LFS: ${LFS:?}"
+
+mkdir -p $LFS/sources 
+
+for f in $(cat $DIST_ROOT/build_env/build_env_list)
+do
+   bn=$(basename $f)
+   echo $bn
+   if ! test -f $LFS/sources/$bn ; then
+      wget $f -O $LFS/sources/$bn  
+   fi
+done;
+
+
+mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin,lib64,tools}
+
+for i in bin lib sbin; do
+  ln -sv usr/$i $LFS/$i
+done
+
+if ! test $(id -u lfs) ; then
+  groupadd lfs
+  useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+  passwd lfs
+  chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,sources}
+
+  dbhome=$(eval echo "~lfs")
+  cat > $dbhome/.bash_profile << "EOF"
+  exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+  cat > $dbhome/.bashrc << EOF
+  set +h  
+  umask 022
+  LFS=$LFS
+  DIST_ROOT=$DIST_ROOT
+EOF
+
+  cat >> $dbhome/.bashrc << "EOF"
+  LC_ALL=POSIX
+  LFS_TGT=$(uname -m)-lfs-linux-gnu
+  PATH=/usr/bin
+  if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+  PATH=$LFS/tools/bin:$PATH
+  CONFIG_SITE=$LFS/usr/share/config.site
+  export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
+  export MAKEFLAGS="-j$(nproc)"
+EOF
+
+  
+fi
